c4099243d5218268e3b8a76699995c2e37c140cc
Arbot-22 <arbabacar@gmail.com>
2022-02-10 01:04:47 +0000

    function fzzBuzz added

---
+++ src/fizzBuzz.js
@@ -0,0 +1,18 @@
+const fizzBuzz = function(number) {
+  //if the value is divisible by 5 and 3 (by 15) FizzBuzz is printed
+  if (number % 3 == 0 && number % 5 == 0) {
+    return 'FizzBuzz';
+    //if the value is divisible by 3 Fizz is printed
+  } else if (number % 3 == 0) {
+    return 'Fizz';
+    //if the value is divisible by 5 Buzz is printed
+  } else if (number % 5 == 0) {
+    return 'Buzz';
+    //if the value is not divisible by 15 or 5 or by 3 the value is printed
+  } else {
+    return number;
+  }
+};
+
+export default fizzBuzz;
+

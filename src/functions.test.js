14319368210c491dc6947fb09394b555665eaf66
Arbot-22 <arbabacar@gmail.com>
2022-02-11 02:28:16 +0000

    Tests refactored using BDD

--- src/functions.test.js
+++ src/functions.test.js
@@ -1,61 +1,151 @@
 import fizzBuzz from './functions/fizzBuzz';
 import fizzBuzz2 from './functions/fizzBuzz2';
-import contains from './functions/util';
+
 
 /* 
-Tests for the function fizzBuzz() :
-Testing the return value of a given integer param  'number'
+*Tests for the function fizzBuzz() :
+*Testing the return value of a given integer param 'number'
 */
 test('1 is returned when number = 1', () => {
-  expect(fizzBuzz(1)).toEqual(1); // 1 not divisible by 3 or 5 or 15
+  //Given 
+  var number = 1;
+
+  // When 
+  var result = fizzBuzz(number);
+
+  //Then
+
+  var expected = 1
+
+  expect(expected).toEqual(result); // 1 not divisible by 3 or 5 or 15
 });
 
 test('Fizz is returned when number = 9', () => {
-  expect(fizzBuzz(9)).toEqual('Fizz'); // 9 divisible by 3
+  //Given 
+  var number = 9;
+
+  // When 
+  var result = fizzBuzz(number);
+
+  //Then
+
+  var expected = "Fizz"
+  expect(expected).toEqual(result); // 9 divisible by 3
 });
 
 test('Buzz is returned when number = 25', () => {
-  expect(fizzBuzz(25)).toEqual('Buzz'); // 25 divisible by 5
+  //Given 
+  var number = 25;
+
+  // When 
+  var result = fizzBuzz(number);
+
+  //Then
+
+  var expected = "Buzz"
+  expect(expected).toEqual(result); // 25 divisible by 5
 });
 
 test('FizzBuzz is returned when number = 30', () => {
-  expect(fizzBuzz(30)).toEqual('FizzBuzz'); // 30 divisible by 15
+  //Given 
+  var number = 30;
+
+  // When 
+  var result = fizzBuzz(number);
+
+  //Then
+
+  var expected = "FizzBuzz"
+  expect(expected).toEqual(result); // 30 divisible by 15
 });
 
 
 /* 
-Tests for the function fizzBuzz2() :
-Testing the return value of a given integer param  'number' 
-    - Using the function contains which return true is the param 'number' contains 
-      a param 'digit'  
+*Tests for the function fizzBuzz2() :
+*Testing the return value of a given integer param  'number'   
 */
 
 test('1 is returned when number = 1', () => {
-  expect(contains(1, 3)).toEqual(false);
-  expect(contains(1, 5)).toEqual(false);
-  expect(fizzBuzz2(1)).toEqual(1); // 1 not divisible by 3 or 5 or 15 and don't contain 3 or 5;
+  //Given 
+  var number = 1;
+
+  // When 
+  var result = fizzBuzz2(number);
+
+  //Then
+
+  var expected = 1
+
+  expect(expected).toEqual(result);; // 1 not divisible by 3 or 5 or 15 and don't contain 3 or 5;
 
 });
 
 test('Fizz is returned when number = 9', () => {
-  expect(fizzBuzz2(9)).toEqual('Fizz'); // 9 divisible by 3
+  //Given 
+  var number = 9;
+
+  // When 
+  var result = fizzBuzz2(number);
+
+  //Then
+
+  var expected = "Fizz"
+
+  expect(expected).toEqual(result); // 9 divisible by 3
 });
 test('Fizz is returned when number = 38 ', () => {
-  expect(contains(38, 3)).toEqual(true);
-  expect(fizzBuzz2(38)).toEqual('Fizz'); // not divisible by 3 but contains 3
+  //Given 
+  var number = 38;
+
+  // When 
+  var result = fizzBuzz2(number);
+
+  //Then
+
+  var expected = "Fizz"
+
+  expect(expected).toEqual(result); // not divisible by 3 but contains 3
 });
 
 test('Buzz is returned when number = 25', () => {
-  expect(fizzBuzz2(25)).toEqual('Buzz'); // 25 divisible by 5
+  //Given 
+  var number = 25;
+
+  // When 
+  var result = fizzBuzz2(number);
+
+  //Then
+
+  var expected = "Buzz"
+
+  expect(expected).toEqual(result); // 25 divisible by 5
 });
 
 test('Buzz is returned when number = 58', () => {
-  expect(contains(58, 3)).toEqual(false);
-  expect(contains(58, 5)).toEqual(true);
-  expect(fizzBuzz2(58)).toEqual('Buzz'); // not divisible by 5 but contains 5 
+  //Given 
+  var number = 58;
+
+  // When 
+  var result = fizzBuzz2(number);
+
+  //Then
+
+  var expected = "Buzz"
+
+  expect(expected).toEqual(result); // not divisible by 5 but contains 5 
 });
 
 test('FizzBuzz is returned when number = 30', () => {
-  expect(fizzBuzz2(30)).toEqual('FizzBuzz'); // 30 divisible by 15
+  //Given 
+  var number = 30;
+
+  // When 
+  var result = fizzBuzz2(number);
+
+  //Then
+
+  var expected = "FizzBuzz"
+
+  expect(expected).toEqual(result); // 30 divisible by 15
 });
 
